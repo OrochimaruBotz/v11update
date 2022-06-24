749cd231e50d37dda3013826c4328d22958e5825
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-25 07:01:01 +0800

    Create anime-asuna.js

---
+++ plugins/anime-asuna.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, command }) => {
+let funix = `https://api-reysekha.herokuapp.com/api/wallpaper/asuna?apikey=APIKEY`
+    conn.sendFile(m.chat, funix, 'Nih', m) 
+}
+handler.help = ['asuna']
+handler.tags = ['anime']
+handler.command = /^(asuna)$/i
+//buatan hyzer, recode by RenFunix
+module.exports = handler
