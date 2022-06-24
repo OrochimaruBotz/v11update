7fb5ffeb50096ad939425cd1e39b662e673d39ed
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-25 01:33:21 +0800

    Update nsfw-hentai.js

--- plugins/nsfw-hentai.js
+++ plugins/nsfw-hentai.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command }) => {
 let funix = `https://api.lolhuman.xyz/api/random/nsfw/hentai?apikey=Papah-Chan`
-    conn.sendButtonImg(m.chat, funix, 'Nih', wm2, 'Next', `.hentai`, m) 
+    conn.sendButtonImg(m.chat, funix, 'Nih', m) 
 }
 handler.help = ['hentai']
 handler.tags = ['nsfw']
