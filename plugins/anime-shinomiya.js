0dc71835a595be793347cb64a339b5f9c17d6a1d
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-25 07:32:46 +0800

    Create anime-shinomiya.js

---
+++ plugins/anime-shinomiya.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, command }) => {
+let funix = `https://api-reysekha.herokuapp.com/api/wallpaper/shinomiya?apikey=APIKEY`
+    conn.sendFile(m.chat, funix, m) 
+}
+handler.help = ['shinomiya']
+handler.tags = ['anime']
+handler.command = /^(shinomiya)$/i
+//buatan hyzer, recode by RenFunix
+module.exports = handler
