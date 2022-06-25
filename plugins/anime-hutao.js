b6fb09a698c1693b14fac6bad11cc9e249025ca1
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-25 10:25:50 +0800

    Update anime-hutao.js

--- plugins/anime-hutao.js
+++ plugins/anime-hutao.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
 	scrap.pinterest("hutao icon","hutao genshin impact","hutao wallpaper","hutao hd wallpaper").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
    }
     
-handler.help = ['animeicon']
+handler.help = ['hutao']
 handler.tags = ['anime']
-handler.command = /^(animeicon)$/i
+handler.command = /^(hutao)$/i
 handler.limit = false
