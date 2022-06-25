fe407815e3405a576008b71909f10ac7e1496c09
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-25 11:34:04 +0800

    Update anime-hutao.js

--- plugins/anime-hutao.js
+++ plugins/anime-hutao.js
@@ -2,7 +2,7 @@ let scrap = require("../lib/scraper_pinterest")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	scrap.pinterest("hutao fanart","hutao genshin impact","hutao wallpaper","hutao icon").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
+	scrap.pinterest("hutao genshin impact","hutao fanart","hutao wallpaper","hutao icon").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
    }
     
 handler.help = ['hutao']
