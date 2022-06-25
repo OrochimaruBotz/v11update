637a4599c2cbd3304f2d5a0be08e13e12f2a6eb8
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-25 09:57:58 +0800

    Update anime-siesta.js

--- plugins/anime-siesta.js
+++ plugins/anime-siesta.js
@@ -2,7 +2,7 @@ let scrap = require("../lib/scraper_pinterest")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	scrap.pinterest("siesta icon","siesta tantei wa mou","siesta tantei wa mou sindeiru","siesta wallpaper","siesta cute icon","siesta tantei wa mou sindeiru wallpaper","siesta hd wallpaper","siesta anime icon").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
+	scrap.pinterest("siesta icon","siesta wallpaper","siesta anime icon","siesta hd wallpaper").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
    }
     
 handler.help = ['siesta']
@@ -10,4 +10,4 @@ handler.tags = ['anime']
 handler.command = /^(siesta)$/i
 handler.limit = false
 
-module.exports = handlerscrap
+module.exports = handler
