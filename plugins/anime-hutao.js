076638c6f99d4575cb133b7566d9e0eca6452de8
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-25 09:43:17 +0800

    Create anime-hutao.js

---
+++ plugins/anime-hutao.js
@@ -0,0 +1,13 @@
+let scrap = require("../lib/scraper_pinterest")
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+	scrap.pinterest("hutao icon","hutao genshin","hutao genshin impact","genshin impact hutao","hutao genshin impact icon","hutao genshin impact wallpaper","hutao wallpaper").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
+   }
+    
+handler.help = ['hutao']
+handler.tags = ['anime']
+handler.command = /^(hutao)$/i
+handler.limit = false
+
+module.exports = handlerscrap
