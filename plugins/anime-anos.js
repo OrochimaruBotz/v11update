dcf40e472f9b62a24f0fb443f973e35ea196eb9f
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-25 11:29:11 +0800

    Update anime-anos.js

--- plugins/anime-anos.js
+++ plugins/anime-anos.js
@@ -2,7 +2,7 @@ let scrap = require("../lib/scraper_pinterest")
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-	scrap.pinterest("anos manga","anos voldigoad","anos icon").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
+	scrap.pinterest("anos voldigoad","anos voldigoad","anos voldigoad icon").then(a => a[Math.floor(Math.random() * a.length)]).then(b => conn.sendFile(m.chat,b,b,"Done",m))
    }
     
 handler.help = ['anos']
