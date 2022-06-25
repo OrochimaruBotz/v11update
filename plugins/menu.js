f763f1335c3c3b371faf86c7d676aea14d87ed06
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-26 02:21:07 +0800

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -59,7 +59,7 @@ const defaultMenu = {
 ──···────────────────···──
 %readmore`.trimStart(),
   header: '╭─㉿ 〔 %category 〕 ㉿─\n┃',
-  body: '› ┃❏ › %cmd %islimit %isPremium',
+  body: '┃❏ › %cmd %islimit %isPremium',
   footer: '┃\n╰────────㉿\n',
   after: `
 ──···────────────────···──
