c84eec9df66df9e091c6462d9756cdfb792be4bf
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-27 05:47:39 +0800

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,10 +1,7 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn }) => {
   let res = await fetch('https://api.zacros.my.id/randomimg/meme')
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'Done', m)
+  conn.sendFile(m.chat, res, '', 'Done', m)
 }
 handler.help = ['meme']
 handler.tags = ['internet']
