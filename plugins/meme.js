b40de1b00e4408c81584cee3389ccb1902127811
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-27 05:52:48 +0800

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command }) => {
 let funix = `https://api.zacros.my.id/randomimg/meme`
-    conn.sendFile(m.chat, funix, 'Done', m) 
+    conn.sendFile(m.chat, funix, 'done', 'Done', m) 
 }
 handler.help = ['meme']
 handler.tags = ['internet']
