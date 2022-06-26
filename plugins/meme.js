22687314961b9cfe2e6de1a560b6ce7bdc85e6a9
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-27 05:51:01 +0800

    Update meme.js

--- plugins/meme.js
+++ plugins/meme.js
@@ -1,11 +1,9 @@
-let fetch = require('node-fetch')
-let handler = async(m, { conn }) => {
-  let res = await fetch('https://api.zacros.my.id/randomimg/meme')
-  conn.sendFile(m.chat, res, '', 'Done', m)
+let handler = async (m, { conn, command }) => {
+let funix = `https://api.zacros.my.id/randomimg/meme`
+    conn.sendFile(m.chat, funix, 'Done', m) 
 }
 handler.help = ['meme']
 handler.tags = ['internet']
-handler.command = /^meme$/i
-handler.limit = false
-
+handler.command = /^(meme)$/i
+//buatan hyzer, recode by RenFunix
 module.exports = handler
