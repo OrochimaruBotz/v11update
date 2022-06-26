1ba6ab4100867d4e5fa647dac0b26ca49f768211
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-27 05:07:37 +0800

    Update kik.js

--- plugins/kik.js
+++ plugins/kik.js
@@ -10,16 +10,12 @@ let fs = require('fs')
   conn.reply(m.chat, wayy, m, { contextInfo: { mentionedJid: users }})
   for (let user of users) if (user.endsWith('@s.whatsapp.net')) await conn.groupParticipantsUpdate(m.chat, [user], "remove")
 }
-handler.help = ['kick'].map(v => v + ' @user')
-handler.tags = ['group']
-handler.command = /^(kick|\-)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
+handler.help = ['okick'].map(v => v + ' @user')
+handler.tags = ['owner']
+handler.command = /^(okick|\-)$/i
+handler.owner = true
 handler.group = true
-handler.private = false
 
-handler.admin = true
 handler.botAdmin = true
 
 handler.fail = null
