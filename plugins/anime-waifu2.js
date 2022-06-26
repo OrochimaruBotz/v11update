73467ad55268ee71621884dd713083cc637fb6b5
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-27 06:08:30 +0800

    Update anime-waifu2.js

--- plugins/anime-waifu2.js
+++ plugins/anime-waifu2.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command }) => {
 let funix = `https://api-reysekha.herokuapp.com/api/wallpaper/waifu2?apikey=APIKEY`
-    conn.sendFile(m.chat, funix, m) 
+    conn.sendFile(m.chat, funix, 'done', 'Done', m) 
 }
 handler.help = ['waifu2']
 handler.tags = ['anime']
