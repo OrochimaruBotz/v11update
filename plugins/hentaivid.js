dc1c839d83d804f8dbe7466658421d98523815f8
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-28 06:08:35 +0800

    Update hentaivid.js

--- plugins/hentaivid.js
+++ plugins/hentaivid.js
@@ -7,9 +7,7 @@ m.reply(wait)
 let res = await fetch(`https://bx-hunter.herokuapp.com/api/randomvidhentai?apikey=d6IZOVLR`)
 let json = await res.json()
 
-conn.sendFile(m.chat, json.result.video_1, 'videohen1.mp4', 'Nih', m, false,{ contextInfo: { forwardingScore: 999, isForwarded: true }})
-
-conn.sendFile(m.chat, json.result.video_2, 'videohen2.mp4', 'nih', m,false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+conn.sendFile(m.chat, json.result.video_1, 'videohen1.mp4', 'Nih', m, true,{ contextInfo: { forwardingScore: 999, isForwarded: true }})
 
 }
 handler.help = [ 'hentaivid']
