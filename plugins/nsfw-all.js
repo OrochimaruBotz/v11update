f1eb68b3678c66ca35e3843866dcfc821cd20cac
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-28 08:38:35 +0800

    Create nsfw-all.js

---
+++ plugins/nsfw-all.js
@@ -0,0 +1,9 @@
+let handler = async (m, { conn, command }) => {
+let funix = `https://zenzapis.xyz/api/morensfw/${command}?apikey=sonelstore`
+    conn.sendFile(m.chat, funix, 'done', 'Done', m) 
+}
+handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri']
+handler.tags = ['nsfw']
+handler.command = /^(ahegao|ass|bdsm|blowjob|cum|ero|femdom|foot|gangbang|glasses|jahy|maid|manga|neko|orgy|panties|pussy|sfwneko|tentacles|thighs|yuri)$/i
+
+module.exports = handler
