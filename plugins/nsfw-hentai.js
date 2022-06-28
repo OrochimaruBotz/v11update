53e5ef0272071071608f383e39fc47fb96a6ffd2
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-06-28 08:43:16 +0800

    Delete nsfw-hentai.js

--- plugins/nsfw-hentai.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, command }) => {
-let funix = `https://api.lolhuman.xyz/api/random/nsfw/hentai?apikey=Papah-Chan`
-    conn.sendFile(m.chat, funix, 'done', 'Done', m)
-}
-handler.help = ['hentai']
-handler.tags = ['nsfw']
-handler.command = /^(hentai)$/i
-//buatan hyzer, recode by RenFunix
-module.exports = handler
