ff1023beee839d85bc4c6ed03abd26c888414838
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-07-02 07:50:42 +0800

    Update vidhentai.js

--- plugins/vidhentai.js
+++ plugins/vidhentai.js
@@ -3,8 +3,8 @@ let handler = async(m, { conn }) => {
   let res = await fetch('https://bx-hunter.herokuapp.com/api/randomvidhentai?apikey=oQpiCHRf')
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.video_1) throw 'Error!'
-  conn.sendFile(m.chat, json.video_1, '', 'Nih', m)
+  if (!json.result) throw 'Error!'
+  conn.sendFile(m.chat, json.result, '', 'Nih', m)
 }
 handler.help = ['vidhentai']
 handler.tags = ['nsfw']
