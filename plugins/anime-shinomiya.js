2e3c1158c78fb0d7a85137417bb22e48c64de9eb
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-07-02 08:12:22 +0800

    Delete anime-shinomiya.js

--- plugins/anime-shinomiya.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, command }) => {
-let funix = `https://api-reysekha.herokuapp.com/api/wallpaper/shinomiya?apikey=APIKEY`
-    conn.sendFile(m.chat, funix, 'done', 'Done', m) 
-}
-handler.help = ['shinomiya']
-handler.tags = ['anime']
-handler.command = /^(shinomiya)$/i
-//buatan hyzer, recode by RenFunix
-module.exports = handler
