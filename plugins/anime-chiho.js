685968f669c90557e275a00e06497f56eaddeab2
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-07-02 08:08:50 +0800

    Delete anime-chiho.js

--- plugins/anime-chiho.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, command }) => {
-let funix = `https://api-reysekha.herokuapp.com/api/wallpaper/chiho?apikey=APIKEY`
-    conn.sendFile(m.chat, funix, 'done', 'Done', m) 
-}
-handler.help = ['chiho']
-handler.tags = ['anime']
-handler.command = /^(chiho)$/i
-//buatan hyzer, recode by RenFunix
-module.exports = handler
