5dc6f94c4db0b6095f85de8333b73776233144ad
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-07-02 08:07:44 +0800

    Delete anime-asuna.js

--- plugins/anime-asuna.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, command }) => {
-let funix = `https://api-reysekha.herokuapp.com/api/wallpaper/asuna?apikey=APIKEY`
-    conn.sendFile(m.chat, funix, 'done', 'Done', m) 
-}
-handler.help = ['asuna']
-handler.tags = ['anime']
-handler.command = /^(asuna)$/i
-//buatan hyzer, recode by RenFunix
-module.exports = handler
