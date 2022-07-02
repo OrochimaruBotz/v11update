acea4c58fd3d3f77997899c3127fb1a460fa6fba
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-07-02 08:10:57 +0800

    Delete anime-tejina.js

--- plugins/anime-tejina.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, command }) => {
-let funix = `https://api-reysekha.herokuapp.com/api/wallpaper/tejina?apikey=APIKEY`
-    conn.sendFile(m.chat, funix, 'done', 'Done', m) 
-}
-handler.help = ['tejina']
-handler.tags = ['anime']
-handler.command = /^(tejina)$/i
-//buatan hyzer, recode by RenFunix
-module.exports = handler
