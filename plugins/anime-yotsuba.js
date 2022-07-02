07bd73f257be178082e6a0372100d42c5895cb72
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-07-02 08:07:18 +0800

    Delete anime-yotsuba.js

--- plugins/anime-yotsuba.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, command }) => {
-let funix = `https://api-reysekha.herokuapp.com/api/wallpaper/yotsuba?apikey=APIKEY`
-    conn.sendFile(m.chat, funix, 'done', 'Done', m) 
-}
-handler.help = ['yotsuba']
-handler.tags = ['anime']
-handler.command = /^(yotsuba)$/i
-//buatan hyzer, recode by RenFunix
-module.exports = handler
