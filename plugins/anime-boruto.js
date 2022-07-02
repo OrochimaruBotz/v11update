e34c9299c903569c9a8609eaecd7019cb4b642b6
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-07-02 08:08:11 +0800

    Delete anime-boruto.js

--- plugins/anime-boruto.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, command }) => {
-let funix = `https://api-reysekha.herokuapp.com/api/wallpaper/boruto?apikey=APIKEY`
-    conn.sendFile(m.chat, funix, 'done', 'Done', m) 
-}
-handler.help = ['boruto']
-handler.tags = ['anime']
-handler.command = /^(boruto)$/i
-//buatan hyzer, recode by RenFunix
-module.exports = handler
