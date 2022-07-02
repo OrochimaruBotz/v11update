2ff2878e49ade54bcb5e22f90c0ad96de0000203
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-07-02 08:10:17 +0800

    Delete anime-kaori.js

--- plugins/anime-kaori.js
+++
@@ -1,9 +0,0 @@
-let handler = async (m, { conn, command }) => {
-let funix = `https://api-reysekha.herokuapp.com/api/wallpaper/kaori?apikey=APIKEY`
-    conn.sendFile(m.chat, funix, 'done', 'Done', m) 
-}
-handler.help = ['kaori']
-handler.tags = ['anime']
-handler.command = /^(kaori)$/i
-//buatan hyzer, recode by RenFunix
-module.exports = handler
