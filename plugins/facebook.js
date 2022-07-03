d31a21b6c3367748fb3aae0da1199612870b7226
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-07-03 15:21:26 +0800

    Update facebook.js

--- plugins/facebook.js
+++ plugins/facebook.js
@@ -1,59 +1,17 @@
-/*let fetch = require('node-fetch')
-const {
-    MessageType
-} = require('@adiwajshing/baileys')
+import { facebookDl } from './scraper.js'
+import { savefrom } from '@bochilteam/scraper'
 
-let handler = async (m, { conn, args, usedPrefix }) => {
-	if (!args[0]) return m.reply('Putting *URL* Facebook..')
-    if (!args[0].includes("facebook")) return m.reply(`Url is wrong..\n\n*Example:*\n${usedPrefix}fb https://www.facebook.com/juankcortavarriaoficial/videos/218237676749570/`)
-	// let res = await fetch(`https://masgimenz.my.id/facebook/?url=` + args[0])
-	let res = await fetch(`https://api.violetics.pw/api/downloader/facebook?apikey=f9f2-4657-e43f&url=` + args[0])
-	//if (res.status !== 200) throw `Coba Lagi`
-	let json = await res.json()
-	//if (!json.result) throw `Media tidak ditemukan atau postingan mungkin diprivate`
-	let url = json.result.hd.url
-	m.reply('Sedang diproses...')
-	if (url) await conn.sendFile(m.chat, url, 'fb.mp4', author, m)
-	//if (url) await conn.sendMessage(m.chat, url, MessageType.video, {mimetype: 'video/mp4', quoted: m, caption: wm})
-	else m.reply('Link download tidak ditemukan')
-	}
-
-handler.help = ['fb'].map(v => v + ' <url>')
+let handler = async (m, { conn, args }) => {
+	if (!args[0]) throw 'Input URL'
+	let res = await facebookDl(args[0]).catch(async _ => await savefrom(args[0])).catch(_ => null)
+	if (!res) throw 'Can\'t download the post'
+	let url = res?.url?.[0]?.url || res?.url?.[1]?.url || res?.['720p'] || res?.['360p']
+	await m.reply('_In progress, please wait..._')
+	conn.sendMessage(m.chat, { video: { url }, caption: res?.meta?.title || '' }, { quoted: m })
+}
+handler.help = ['facebook']
 handler.tags = ['downloader']
+handler.alias = ['fb', 'fbdl', 'facebook', 'facebookdl']
+handler.command = /^((facebook|fb)(dl)?)$/i
 
-handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
-handler.limit = true
-handler.group = true
-handler.premium = false
-
-module.exports = handler*/
-
-
-let fetch = require('node-fetch')
-const {
-    MessageType
-} = require('@adiwajshing/baileys')
-
-let handler = async (m, { conn, args, usedPrefix }) => {
-	if (!args[0]) return m.reply('Putting *URL* Facebook..')
-    if (!args[0].includes("facebook")) return m.reply(`Url is wrong..\n\n*Example:*\n${usedPrefix}fb https://www.facebook.com/juankcortavarriaoficial/videos/218237676749570/`)
-	let res = await fetch(`https://masgimenz.my.id/facebook/?url=` + args[0])
-	//if (res.status !== 200) throw `Coba Lagi`
-	let json = await res.json()
-	//if (!json.result) throw `Media tidak ditemukan atau postingan mungkin diprivate`
-	let url = json.videoUrl
-	m.reply('Sedang diproses...')
-	if (url) await conn.sendFile(m.chat, url, 'fb.mp4', wm, m)
-	//if (url) await conn.sendMessage(m.chat, url, MessageType.video, {mimetype: 'video/mp4', quoted: m, caption: wm})
-	else m.reply('Link download tidak ditemukan')
-	}
-
-handler.help = ['fb'].map(v => v + ' <url>')
-handler.tags = ['downloader']
-
-handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
-handler.limit = false
-handler.group = true
-handler.premium = false
-
-module.exports = handler
+export default handler
