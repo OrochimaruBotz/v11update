1c39d4fb1f0d0a0c3fde22b181ca40d0e362c589
RenFunix <106660279+RenFunix@users.noreply.github.com>
2022-07-04 16:32:29 +0800

    Update fun-benarkah.js

--- plugins/fun-benarkah.js
+++ plugins/fun-benarkah.js
@@ -1,13 +1,17 @@
-let handler = async (m) => m.reply(`
-*Pertanyaan:* ${m.text}
-*Jawaban:* ${['Iya','Sudah pasti','Sudah pasti bisa','Tidak','Tentu tidak','Sudah pasti tidak'].getRandom()}
+let handler = async (m, { command, text }) => {
+let jawab = ['Iya','Sudah pasti','Sudah pasti bisa','Tidak','Tentu tidak','Sudah pasti tidak']
+let siapa = jawab[Math.floor(Math.random() * jawab.length)]
+m.reply(`
+*Pertanyaan:* ${command} ${text}
+*Jawaban:* ${siapa}
   `.trim(), null, m.mentionedJid ? {
   mentions: m.mentionedJid
 } : {})
-
+}
 handler.help = ['benarkah <teks>?']
 handler.tags = ['kerang']
-handler.customPrefix = /(\?$)/
-handler.command = /^benarkah$/i
 
-export default handler
+handler.command = /^benarkah$/i
+handler.limit = false
+
+module.exports = handler
